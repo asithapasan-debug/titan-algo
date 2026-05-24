efec8352b446a4f3bb399b2b1c032ee642aa1d46
Asitha <asithapasan@example.com>
2026-05-24 13:42:30 +0530

    Increase minimum score to 15 to drastically improve win rate

--- server_engine.js
+++ server_engine.js
@@ -171,19 +171,19 @@ const Engine = {
         this.DB.sigs1h = purgeOld(this.DB.sigs1h);
 
         const sig5 = genSig(c5, c15, c1h, c1d, fr, '5m', sym);
-        if (sig5 && sig5.sc >= 10 && !this.DB.sigs5.find(s => s.id === sig5.id)) {
+        if (sig5 && sig5.sc >= 15 && !this.DB.sigs5.find(s => s.id === sig5.id)) {
             this.DB.sigs5.unshift(sig5);
             if (this.onNewSignal) this.onNewSignal(sig5);
         }
         
         const sig15 = genSig(c15, c1h, c4h, c1d, fr, '15m', sym);
-        if (sig15 && sig15.sc >= 10 && !this.DB.sigs15.find(s => s.id === sig15.id)) {
+        if (sig15 && sig15.sc >= 15 && !this.DB.sigs15.find(s => s.id === sig15.id)) {
             this.DB.sigs15.unshift(sig15);
             if (this.onNewSignal) this.onNewSignal(sig15);
         }
         
         const sig1h = genSig(c1h, c4h, c1d, c1d, fr, '1h', sym);
-        if (sig1h && sig1h.sc >= 10 && !this.DB.sigs1h.find(s => s.id === sig1h.id)) {
+        if (sig1h && sig1h.sc >= 15 && !this.DB.sigs1h.find(s => s.id === sig1h.id)) {
             this.DB.sigs1h.unshift(sig1h);
             if (this.onNewSignal) this.onNewSignal(sig1h);
         }
